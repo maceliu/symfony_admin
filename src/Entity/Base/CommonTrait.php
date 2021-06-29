8ea3b1a7c66a46858de9bf4edb142efc820865f6
mace liu <liubo@xianglin.cn>
2021-06-29 15:37:05 +0800

    优化更新事件触发器机制

--- src/Entity/Base/CommonTrait.php
+++ src/Entity/Base/CommonTrait.php
@@ -5,16 +5,50 @@ namespace SymfonyAdmin\Entity\Base;
 
 
 use DateTime;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\ORM\Mapping as ORM;
+use SymfonyAdmin\Utils\Enum\OperateEnum;
 
 trait CommonTrait
 {
+    /** @var string */
+    private $operateStatus = OperateEnum::NONE;
+
     /**
-     * @ORM\PrePersist
+     * @return string
+     */
+    public function getOperateStatus(): string
+    {
+        return $this->operateStatus;
+    }
+
+    /**
+     * @param string $operateStatus
+     */
+    public function setOperateStatus(string $operateStatus): void
+    {
+        $this->operateStatus = $operateStatus;
+    }
+
+    /**
+     * @param PreUpdateEventArgs $event
      * @ORM\PreUpdate
      */
-    public function postUpdate()
+    public function preUpdate(PreUpdateEventArgs $event)
     {
         $this->setUpdateTime(new DateTime());
+        if (!empty($event->getEntityChangeSet())) {
+            $this->operateStatus = OperateEnum::UPDATE;
+        }
+    }
+
+    /**
+     * @ORM\PostPersist
+     */
+    public function postPersist()
+    {
+        if ($this->getCreateTime()->getTimestamp() == $this->getUpdateTime()->getTimestamp()) {
+            $this->operateStatus = OperateEnum::CREATE;
+        }
     }
 }
---
+++ src/Utils/Enum/OperateEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace SymfonyAdmin\Utils\Enum;
+
+
+class OperateEnum
+{
+    const UPDATE = 'update';
+
+    const CREATE = 'create';
+
+    const NONE = 'none';
+}
