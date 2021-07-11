523d4bd9f617ad470d524cfe020c94b7f0654c00
mace liu <liubo@xianglin.cn>
2021-07-11 08:58:23 +0800

    优化异常处理

--- src/Exception/Base/BaseException.php
+++ src/Exception/Base/BaseException.php
@@ -3,11 +3,12 @@
 
 namespace SymfonyAdmin\Exception\Base;
 
+use Exception;
 
 /**
  * 代码类阻断抛出的异常，例如数据库连接失败、Redis连接失败、外部接口调用失败等
  */
-class ErrorException extends BaseException
+class BaseException extends Exception
 {
 
 }
--- src/Exception/Base/ServiceException.php
+++ src/Exception/Base/ServiceException.php
@@ -6,7 +6,7 @@ namespace SymfonyAdmin\Exception\Base;
 /**
  * 业务类阻断抛出的异常，例如传入参数缺失、数据格式错误、数据不存在、检查未通过等
  */
-class ServiceException extends \Exception
+class ServiceException extends BaseException
 {
 
 }
